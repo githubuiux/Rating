e074e325f86e84be3c9e63d911e1553777033468
githubuiux <uiuxista@gmail.com>
2023-09-06 12:54:29 +0300

    Initial Commit

---
+++ Rating/RatingApp.swift
@@ -0,0 +1,17 @@
+//
+//  RatingApp.swift
+//  Rating
+//
+//  Created by Levent on 6.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct RatingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
